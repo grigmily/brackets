1956430cec4230a65e5eb49057385aed11127015
grigmily <grigmily@gmail.com>
2022-02-02 20:29:50 +0300

    feat: task solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bset = bracketsConfig.reduce((a,v)=>a+= v[0]+v[1], '');
+    let st = [];
+    for (let i in str) {
+        let idx = bset.indexOf(str[i]);
+        let clone = JSON.parse(JSON.stringify(idx))
+        if (bset[idx]===bset[idx+1]){  //check if it is non-paired
+            if (st[st.length-1]!==idx) st.push(clone) 
+            else st.pop()    
+        }
+        else if (idx%2 === 0) {
+            st.push(clone);
+        }
+            else 
+                if (st.pop() !== (idx-1)) {
+                    return false
+                }
+    }
+    return !st.length
 }
